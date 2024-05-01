619348bf2d8f28a10ee13b1922824d05d77317ba
Promyse Ferguson <promyseferguson@gmail.com>
2024-05-01 20:58:41 +0000

    quick update

--- password_cracker.py
+++ password_cracker.py
@@ -1,10 +1,19 @@
 import hashlib
 hashed_pw = input("Please provide the hash:\n")
 def crack_sha1_hash(hash, use_salts = False):
-    with open("top-10000-passwords.txt", 'r') as f:
-        for line in f:
-            if hash == hashlib.sha1(line.strip().encode()).hexdigest():
-                return line.strip()
+    if use_salts == False:
+        with open("top-10000-passwords.txt", 'r') as f:
+            for line in f:
+                if hash == hashlib.sha1(line.strip().encode()).hexdigest():
+                 return line.strip()
+    else:
+        with open("top-10000-passwords.txt", 'r') as f:
+            for line in f:
+                if hash == hashlib.sha1(line.strip().encode()).hexdigest():
+                    with open("known-salts.txt", "r") as salts:
+                        for x in salts:
+                            hash = x + hash + x 
+        return hash
     return "PASSWORD NOT IN DATABASE"
 
 
