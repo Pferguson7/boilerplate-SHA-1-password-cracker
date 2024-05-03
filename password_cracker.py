25ffc7d2529e9fa71e3a7841ac116fad0ba27e6f
unknown <promyseferguson@gmail.com>
2024-05-03 07:33:04 -0400

    quick update

--- password_cracker.py
+++ password_cracker.py
@@ -1,16 +1,26 @@
 import hashlib
 
+from matplotlib import use
+
 hashed_pw = input("Please provide the hash:\n")
 def crack_sha1_hash(hash, use_salts = False):
     with open("top-10000-passwords.txt", "r") as f:
-        passwords = f.readlines()
+        passwords = [password.strip() for password in f.readlines()]
 
     with open("known-salts.txt", "r") as salt_file:
-        salts = salt_file.readlines() 
-    
-    for password in passwords:
-        if hash == hashlib.sha1(password.strip().encode()).hexdigest():
-            return password.strip()
+        salts = [salt.strip() for salt in salt_file.readlines()]
+
+    if use_salts:
+        for password in passwords:
+            for salt in salts:
+                for salted_password in [salt + password, password + salt]:
+                    hashed_pw = hashlib.sha1(salted_password.encode()).hexdigest()
+                    if hash == hashed_pw:
+                        return password
+    else:
+        for password in passwords:
+            if hash == hashlib.sha1(password.strip().encode()).hexdigest():
+                return password.strip()
   
 
     return "PASSWORD NOT FOUND IN DATABASE"
